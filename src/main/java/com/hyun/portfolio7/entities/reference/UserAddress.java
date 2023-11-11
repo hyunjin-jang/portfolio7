afe4d18d7992193e293c6d0cf2695fbd784eeeec
hyun <okmt0423@gmail.com>
2023-11-11 21:29:26 +0900

    원상복구

--- src/main/java/com/hyun/portfolio7/entities/reference/UserAddress.java
+++ src/main/java/com/hyun/portfolio7/entities/reference/UserAddress.java
@@ -1,5 +1,10 @@
 package com.hyun.portfolio7.entities.reference;
 
+import jakarta.persistence.Embeddable;
+import lombok.Getter;
+
+@Embeddable
+@Getter
 public class UserAddress {
     private String userAddress;
     private String UserAddressDetail;
--- src/main/java/com/hyun/portfolio7/entities/reference/UserName.java
+++ src/main/java/com/hyun/portfolio7/entities/reference/UserName.java
@@ -1,5 +1,10 @@
 package com.hyun.portfolio7.entities.reference;
 
+import jakarta.persistence.Embeddable;
+import lombok.Getter;
+
+@Embeddable
+@Getter
 public class UserName {
     private String firstName;
     private String lastName;
